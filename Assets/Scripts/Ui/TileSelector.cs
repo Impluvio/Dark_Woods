678ebcab61e56c3c0f8a5fdba59e3da0c76c520a
Adam Tuffey <adamcorneliustuffey@gmail.com>
2025-08-17 21:41:21 +0100

    added change material functionality

--- Assets/Scripts/Ui/TileSelector.cs
+++ Assets/Scripts/Ui/TileSelector.cs
@@ -1,26 +1,52 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TileSelector : MonoBehaviour
 {
 
+    public Material SelectionTexture;
 
-    void OnEnable()
+    string MapTile = "MapTile";
+
+    private void OnEnable() => RayCastHandler.OnRayCast += selectTile;
+
+    private void OnDisable() => RayCastHandler.OnRayCast -= selectTile;
+
+    public void selectTile(Ray ray)
     {
+        GameObject gameTileToProcess;
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            string objectName = hit.collider.name;
+            Debug.Log(objectName);
+
+            gameTileToProcess = hit.collider.gameObject;
+
+            if (gameTileToProcess.tag == MapTile)
+            {
+                changeMaterial(gameTileToProcess);
+            }
+
+
+        }
 
     }
 
-    void OnDisable()
+    private void changeMaterial(GameObject tile)
     {
-        
+        Renderer tileRenderer = tile.GetComponent<Renderer>();
+
+        if (tileRenderer != null)
+        {
+            Material[] materials = tileRenderer.materials;
+
+            materials[0] = SelectionTexture;
+
+            tileRenderer.materials = materials;
+        }
+
+
     }
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    
 }
