58279651390aabb2a1393bd0043c1dd90eb4a3cf
Adam Tuffey <adamcorneliustuffey@gmail.com>
2025-08-17 12:17:42 +0100

    seperated raycast handling from plane selector script

--- Assets/Scripts/Inputs & Interactions/PlaneSelector.cs
+++ Assets/Scripts/Inputs & Interactions/PlaneSelector.cs
@@ -12,97 +12,51 @@ public class PlaneSelector : MonoBehaviour
     public ARRaycastManager rayCastManager;
     public ARPlaneManager planeManager;
     public ARAnchorManager anchorManager;
-
-    private InputAction tapAction;
-    public Camera arCamera;
-    private ARAnchor playAreaAnchor;
-    [SerializeField] private GameObject Marker;
-    private GameObject liveMarker;
-
     public MapCreator mapCreator;
     public UiManager uiManager;
+
+    [SerializeField] private GameObject Marker;
+
+    private ARAnchor playAreaAnchor;
+    private GameObject liveMarker;
+  
     public TrackableId playAreaID;
-
     private bool playAreaSelected = false;
-
     private List<ARRaycastHit> hits = new();
-    public GameObject origin { get; set; }
 
-    private void Awake()
+
+    private void OnEnable() => RayCastHandler.OnRayCast += HandleRayCast;
+    private void OnDisable() => RayCastHandler.OnRayCast -= HandleRayCast;
+  
+
+    void HandleRayCast(Ray ray)
     {
-        tapAction = new InputAction(type: InputActionType.PassThrough);
-        tapAction.AddBinding("<Touchscreen>/primaryTouch/press");
-        tapAction.AddBinding("<Mouse>/leftButton");
-    }
+        if (playAreaSelected) return;
 
-    private void OnEnable()
-    {
-        tapAction.Enable();
-        tapAction.performed += onTapPerformed;
-    }
-
-    private void OnDisable()
-    {
-        tapAction.performed -= onTapPerformed;
-        tapAction.Disable();
-    }
-
-    public void onTapPerformed(InputAction.CallbackContext context)
-    {
-        Vector2 screenPosition;
-
-        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
-        {
-            screenPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-        }
-        else if (Mouse.current != null && Mouse.current.leftButton.isPressed)
-        {
-            screenPosition = Mouse.current.position.ReadValue();
-        }
-        else
-        {
-            return;
-        }
-
-        //Sets the anchor and sets play area to true.
-
-        if (rayCastManager.Raycast(screenPosition, hits, TrackableType.Planes) && !playAreaSelected)
+        if (rayCastManager.Raycast(ray, hits, TrackableType.Planes))
         {
             ARRaycastHit hit = hits[0];
-
-            TrackableId planeID = hit.trackableId;
+            TrackableId planeID = hit.trackableId; // plane to process
 
             if (planeManager.trackables.TryGetTrackable(planeID, out ARPlane plane))
             {
-                Vector3 worldCentre = plane.transform.TransformPoint(plane.center);
-                Pose centerPose = new Pose(worldCentre, plane.transform.rotation);
+                Pose centerPoseOfPlane = new Pose(plane.transform.TransformPoint(plane.center), plane.transform.rotation);
 
-                AttachAnchor(anchorManager, plane, centerPose);
-                            //printPlaneID.PrintMessage("Transform of anchor:" + playAreaAnchor.transform.position);
+                AttachAnchor(anchorManager, plane, centerPoseOfPlane);
+                liveMarker = Instantiate(Marker, playAreaAnchor.transform);
+                uiManager.displayQueryPlacement(true);
                 playAreaID = playAreaAnchor.trackableId;
-                liveMarker = Instantiate(Marker, playAreaAnchor.transform); // this works but can be repeated ad infinitum - needs to be once
-                confirmGamePlayLocation(true);
-                            //Todo: turn off planes, once anchor and grid is established. 
-                            //Add confirm play area rule so that users can instantiate grid elsewhere. 
-
-
             }
-        }
-    }
 
+        }
+
+    }
     void AttachAnchor(ARAnchorManager arAnchorManager, ARPlane plane, Pose pose)
     {
         if (arAnchorManager.descriptor.supportsTrackableAttachments)
         {
             playAreaAnchor = arAnchorManager.AttachAnchor(plane, pose);
-
         }
-    }
-
-    private void confirmGamePlayLocation(bool turnOn)
-    {
-        uiManager.displayQueryPlacement(turnOn);
-
     }
 
     public void setPlayArea(bool areaSet)
@@ -110,7 +64,7 @@ public class PlaneSelector : MonoBehaviour
         if (!areaSet)
         {
             Destroy(liveMarker);
-            confirmGamePlayLocation(false);
+            uiManager.displayQueryPlacement(false);
 
 
         }
@@ -118,7 +72,7 @@ public class PlaneSelector : MonoBehaviour
         {
             mapCreator.InitialiseMap(playAreaID);
             Debug.Log("area selected");
-            confirmGamePlayLocation(false);
+            uiManager.displayQueryPlacement(false);
             playAreaSelected = true;
             turnOffPlanes();
         }
--- Assets/Scripts/Inputs & Interactions/RayCastHandler.cs
+++ Assets/Scripts/Inputs & Interactions/RayCastHandler.cs
@@ -1,16 +1,56 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class RayCastHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Camera arCamera;
+    public InputAction tapAction;
+
+    public static event Action<Ray> OnRayCast;
+
+    private void Awake()
     {
-        
+        tapAction = new InputAction(type: InputActionType.PassThrough);
+        tapAction.AddBinding("<Touchscreen>/primaryTouch/press");
+        tapAction.AddBinding("<Mouse>/leftButton");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        tapAction.Enable();
+        tapAction.performed += HandleTap;
     }
+
+    void OnDisable()
+    {
+        tapAction.performed -= HandleTap;
+        tapAction.Disable();
+    }
+
+    private void HandleTap(InputAction.CallbackContext context)
+    {
+        Vector2 screenPosition;
+
+        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            screenPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        }
+        else if (Mouse.current != null && Mouse.current.leftButton.isPressed)
+        {
+            screenPosition = Mouse.current.position.ReadValue();
+        }
+        else
+        {
+            return;
+        }
+
+        Ray ray = arCamera.ScreenPointToRay(screenPosition);
+        OnRayCast?.Invoke(ray);
+
+    }
+
+
 }
