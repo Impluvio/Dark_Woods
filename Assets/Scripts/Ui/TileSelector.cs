4524c05a11ad8f367cf387d00dc23b47928f003b
Adam Tuffey <adamcorneliustuffey@gmail.com>
2025-08-17 10:20:50 +0100

    Commit before refactoring & tidying code

--- Assets/Scripts/Ui/TileSelector.cs
+++ Assets/Scripts/Ui/TileSelector.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TileSelector : MonoBehaviour
 {
+
+
+    void OnEnable()
+    {
+
+    }
+
+    void OnDisable()
+    {
+        
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
